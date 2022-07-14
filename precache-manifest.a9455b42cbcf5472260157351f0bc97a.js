b30ccb35ec2ff16891cfcd18a434a1d4aeb44943
Bogagree <bogatireshka@gmail.com>
2022-07-14 14:53:01 +0300

    Updates

---
+++ precache-manifest.a9455b42cbcf5472260157351f0bc97a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2cd3df36e7c50dbd434b283abeea644",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23f0e7bb6b7c41795052",
+    "url": "/static/css/main.a8cdb5e3.chunk.css"
+  },
+  {
+    "revision": "c7d1a856197086f55e19",
+    "url": "/static/js/2.25ce50d2.chunk.js"
+  },
+  {
+    "revision": "f82f5fd7757727752623fccdba05e8e5",
+    "url": "/static/js/2.25ce50d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23f0e7bb6b7c41795052",
+    "url": "/static/js/main.1f8f8c1d.chunk.js"
+  },
+  {
+    "revision": "ad2af91736b280adb865",
+    "url": "/static/js/runtime-main.f123d06a.js"
+  }
+]);
