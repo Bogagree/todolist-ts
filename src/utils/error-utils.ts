38b449a7bdd9c1145e1c2da2428d892f74b2caee
Bogagree <bogatireshka@gmail.com>
2022-12-11 14:54:23 +0300

    fix: add error:AxiosError for  handelServerAppError

--- src/utils/error-utils.ts
+++ src/utils/error-utils.ts
@@ -1,20 +1,21 @@
 import {setAppError, SetAppErrorActionType, setAppStatus, SetAppStatusActionType} from '../app/app-reducer';
 import {ResponseType} from '../api/todolists-api';
 import {Dispatch} from 'redux';
+import {AxiosError} from 'axios';
 
 export const handelServerAppError = <D>(data: ResponseType<D>, dispatch: Dispatch) => {
     if (data.messages.length) {
-        dispatch(setAppError({error: data.messages[0]}))
+        dispatch(setAppError(data.messages[0]))
     } else {
-        dispatch(setAppError({error: 'Some error occurred'}))
+        dispatch(setAppError('Some error occurred'))
     }
-    dispatch(setAppStatus({status: 'failed'}))
+    dispatch(setAppStatus('failed'))
 }
 
-export const handleServerNetworkError = (error: { error: string }, dispatch: Dispatch<SetAppErrorActionType | SetAppStatusActionType>) => {
-    console.log(error)
-    dispatch(setAppError(error.error.length ? error : {error: 'Some error occurred'}))
-    dispatch(setAppStatus({status: 'failed'}))
+export const handleServerNetworkError = (error: AxiosError, dispatch: Dispatch<SetAppErrorActionType | SetAppStatusActionType>) => {
+    console.log(error.message)
+    dispatch(setAppError(error ? error.message : 'Some error occurred'))
+    dispatch(setAppStatus('failed'))
     // dispatch(setAppError({error: error.message}))
     // dispatch(setAppStatus({status: 'failed'}))
 }
